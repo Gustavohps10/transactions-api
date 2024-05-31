a55acfd50d20dcde037b328105513195bb0365ac
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-05-31 17:09:22 -0300

    feat: add route to create transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,9 +1,24 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { knex } from "../database";
+import { randomUUID } from "crypto";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/hello", async () => {
-    const transaction = await knex("transactions").select("*");
-    return transaction;
+  app.post("/", async (req, res) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(req.body);
+
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : -amount,
+    });
+
+    return res.status(201).send();
   });
 }
