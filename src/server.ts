f00ed8d33434556cfd1b41eeb5b9d4eabe830fb3
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-05-31 15:22:47 -0300

    feat: separate transaction routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
 import fastify from "fastify";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify();
-
-app.get("/hello", async () => {
-  const transaction = await knex("transactions").select("*");
-  return transaction;
-});
-
+app.register(transactionsRoutes);
 app
   .listen({ port: env.PORT })
   .then(() => console.log(`Server Running on http://localhost:${env.PORT}`));
