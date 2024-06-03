fc06ee5e33d48b996440ce17eff8c989ef2f2096
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-03 05:02:58 -0300

    feat: add route - summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -11,6 +11,13 @@ export async function transactionsRoutes(app: FastifyInstance) {
     };
   });
 
+  app.get("/summary", async () => {
+    const summary = await knex("transactions")
+      .sum("amount", { as: "amount" })
+      .first();
+    return { summary };
+  });
+
   app.get("/:id", async (req) => {
     const getTransactionZodSchema = z.object({
       id: z.string().uuid(),
