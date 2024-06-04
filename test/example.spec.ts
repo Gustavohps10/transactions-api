36aecb1b80e3d96cec3c3e0893caf56a1bb3f965
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-04 06:27:05 -0300

    test: add vitest config

---
+++ test/example.spec.ts
@@ -0,0 +1,21 @@
+import { test, beforeAll, afterAll } from "vitest";
+import request from "supertest";
+import { app } from "../src/app";
+
+beforeAll(async () => {
+  await app.ready();
+});
+
+afterAll(async () => {
+  await app.close();
+});
+test("Create Transaction", async () => {
+  await request(app.server)
+    .post("/transactions")
+    .send({
+      title: "New transaction",
+      amount: 5000,
+      type: "credit",
+    })
+    .expect(201);
+});
