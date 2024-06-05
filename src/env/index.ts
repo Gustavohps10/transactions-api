6aecc2b2bd51ac25ae3a0cdbc3a0d27fa1f5c37a
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-05 06:38:24 -0300

    chore: production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,8 +2,9 @@ import z from "zod";
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
+  DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   DATABASE_URL: z.string(),
-  PORT: z.number({ coerce: true }).int().default(3333),
+  PORT: z.coerce.number().default(3333),
 });
 
 export const env = envSchema.parse(process.env);
