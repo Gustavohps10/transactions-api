f2fb6217ca4bddaa7da30fd5ea8cc974d24601a4
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-05 07:15:36 -0300

    fix: host env

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
   DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
   DATABASE_URL: z.string(),
+  HOST: z.string().default("localhost"),
   PORT: z.coerce.number().default(3333),
 });
 
--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,8 @@ import { app } from "./app";
 import { env } from "./env";
 
 app
-  .listen({ port: env.PORT })
-  .then(() => console.log(`Server Running on http://localhost:${env.PORT}`));
+  .listen({
+    host: env.HOST,
+    port: env.PORT,
+  })
+  .then(() => console.log(`Server Running on http://${env.HOST}:${env.PORT}`));
