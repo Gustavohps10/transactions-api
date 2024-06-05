edc63c9db7906a8be4a428fa024a0e78c0c0e656
Gustavo Henrique <gustavoh.santos735@gmail.com>
2024-06-05 02:45:12 -0300

    test: should be able to list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from "vitest";
+import { beforeAll, afterAll, describe, it, expect } from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
 
@@ -11,7 +11,7 @@ describe("Transactions routes", () => {
     await app.close();
   });
 
-  test("should be able to create a new transaction", async () => {
+  it("should be able to create a new transaction", async () => {
     await request(app.server)
       .post("/transactions")
       .send({
@@ -20,5 +20,35 @@ describe("Transactions routes", () => {
         type: "debit",
       })
       .expect(201);
+  });
+
+  it("should be able to list all transactions", async () => {
+    const createTransactionResponse = await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New transaction",
+        amount: 5000,
+        type: "debit",
+      });
+
+    const cookies = createTransactionResponse.get("Set-Cookie");
+
+    if (!cookies) {
+      throw new Error("Cookies not found");
+    }
+
+    const listTransactionsResponse = await request(app.server)
+      .get("/transactions")
+      .set("Cookie", cookies)
+      .expect(200);
+
+    console.log(listTransactionsResponse.body.transactions);
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: "New transaction",
+        amount: -5000,
+      }),
+    ]);
   });
 });
